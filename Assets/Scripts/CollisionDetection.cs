964aeb465c1fd5e8d06a6cf0d8dc09b969cdac18
Drew Donovan <ddd5189@rit.edu>
2020-02-26 20:02:03 -0500

    implemented basic puzzle
    If the player hits the cylinder with the sphere it will toggle the visibility of the capsule next to it. Also, if the player has the sphere in their inventory and they click on the cylinder it will also activate the event.

---
+++ Assets/Scripts/CollisionDetection.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+
+    // public field
+    public GameObject key;
+    public GameObject open;
+
+    public Invetory In;
+    // Start is called before the first frame update
+    void Start()
+    {
+        open.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.position == key.transform.position)
+        {
+            if (open.activeSelf == false)
+            {
+                open.SetActive(true);
+            }
+            else
+            {
+                open.SetActive(false);
+            }
+        }
+
+    }
+
+    void OnMouseDown()
+    {
+        for (int i = 0; i < In.invetory.Count; i++)
+        {
+            if (In.invetory[i] == key)
+            {
+                if (open.activeSelf == false)
+                {
+                    open.SetActive(true);
+                }
+                else
+                {
+                    open.SetActive(false);
+                }
+            }
+        }
+    }
+}
