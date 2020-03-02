775bdd75a3adfa28b6ce6c07383e797bc041eb97
Trevor Seiders <31632349+TrevorSeiders@users.noreply.github.com>
2020-03-02 01:20:19 -0500

    Very basic implementation of World State transition functionality.

---
+++ Assets/Scripts/TimeShifter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeShifter : MonoBehaviour
+{
+    public int currentWorldState = 0;
+    public int maxWorldState = 2;
+
+    public List<GameObject> world0Objects;
+    public List<GameObject> world1Objects;
+
+    public Dictionary<int, List<GameObject>> worldStateObjects;
+
+    void Start()
+    {
+        worldStateObjects = new Dictionary<int, List<GameObject>>();
+        worldStateObjects.Add(0, world0Objects);
+        worldStateObjects.Add(1, world1Objects);
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            int newWorldState = (currentWorldState + 1) % maxWorldState;
+            ToggleWorldState(newWorldState);
+        }
+    }
+
+    private void ToggleWorldState(int newWorldState)
+    {
+        foreach ( GameObject obj in worldStateObjects[currentWorldState])
+        {
+            obj.SetActive(false);
+        }
+        foreach ( GameObject obj in worldStateObjects[newWorldState])
+        {
+            obj.SetActive(true);
+        }
+
+        currentWorldState = newWorldState;
+    }
+}
