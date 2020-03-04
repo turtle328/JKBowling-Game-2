77b99604a9372c1dab05cbfbf24bbdae986ee797
ajr6974 <ajr6974@g.rit.edu>
2020-03-04 17:01:12 -0500

    Added zoom. Hold ctrl to zoom

---
+++ Assets/Scripts/Zoom.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zoom : MonoBehaviour
+{
+    Camera playerCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCamera = GetComponent<Camera>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            playerCamera.fieldOfView = 15;
+        }
+        else
+        {
+            playerCamera.fieldOfView = 60;
+        }
+    }
+}
