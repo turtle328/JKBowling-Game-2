8fe44e56ccbb857784b0751d831d38003af1caa8
Drew Donovan <ddd5189@rit.edu>
2020-05-03 19:12:09 -0400

    Working puzzle 3
    puzzle 3 works, it is completed when the diary is removed from the users inventory

--- Assets/Scripts/Scene3Manager.cs
+++ Assets/Scripts/Scene3Manager.cs
@@ -6,13 +6,54 @@ using UnityEngine;
 
 public class Scene3Manager : MonoBehaviour
 {
-
+    // instances of the pages
     public GameObject page1;
     public GameObject page2;
     public GameObject page3;
     public GameObject lastPage;
+
+    // next and prev buttons
+    public GameObject prevButton;
+    public GameObject nextButton;
+    // minus and plus 
+    public GameObject plusB0;
+    public GameObject plusB1;
+    public GameObject plusB2;
+    public GameObject minusB0;
+    public GameObject minusB1;
+    public GameObject minusB2;
+    // puzzle 1 inputs
+    public GameObject inputP1_1;
+    public GameObject inputP1_2;
+    public GameObject inputP1_3;
+    public GameObject inputP1_4;
+    public GameObject inputP1_Answer;
+    // puzzle 2 input
+    public GameObject inputP2_Answer;
+    // puzzle 3 inputs
+    public GameObject inputP3_1;
+    public GameObject inputP3_2;
+    public GameObject inputP3_3;
+    public GameObject inputP3_4;
+    public GameObject inputP3_Answer;
+    // final input
+    public GameObject inputFinal;
+    
+    public GameObject player;
+    public GameObject diary;
+
+    // counter for pages
+    public int page = 0;
+
+    public bool diaryOpen = false;
+
+    // access to the inventory
+    public Inventory inven;
+
+    // page dimension constants
     const int pageWidth = 675;
     const int pageHeight = 900;
+    // puzzle textures and dimensions
     public Texture2D puzzle1;
     private int puzzle1Width = 600;
     private int puzzle1Height = 50;
@@ -22,6 +63,7 @@ public class Scene3Manager : MonoBehaviour
     public Texture2D puzzle3;
     private int puzzle3Width = 600;
     private int puzzle3Height = 50;
+    // extra textures for puzzles
     public Texture2D minusCode;
     public Texture2D plusCode;
     private int mpWidth = 200;
@@ -44,46 +86,12 @@ public class Scene3Manager : MonoBehaviour
     public Texture2D signCode;
     private int signCodeWidth = 100;
     private int signCodeHeight = 100;
-    //public Texture2D nextButton;
-    //private int nextButtonWidth = 675;
-    //private int nextButtonHeight = 900;
-    //public Texture2D prevButton;
-    //private int prevButtonWidth = 675;
-    //private int prevButtonHeight = 900;
-    public GameObject player;
-    public GameObject diary;
 
-    public int page = 0;
-
-    public GameObject prevButton;
-    public GameObject nextButton;
-
-    public GameObject inputP1_1;
-    public GameObject inputP1_2;
-    public GameObject inputP1_3;
-    public GameObject inputP1_4;
-    public GameObject inputP1_Answer;
-
-    public GameObject inputP2_Answer;
-
-    public GameObject inputP3_1;
-    public GameObject inputP3_2;
-    public GameObject inputP3_3;
-    public GameObject inputP3_4;
-    public GameObject inputP3_Answer;
-
-    public GameObject inputFinal;
-
-    public GameObject plusB0;
-    public GameObject plusB1;
-    public GameObject plusB2;
-    public GameObject minusB0;
-    public GameObject minusB1;
-    public GameObject minusB2;
     // Start is called before the first frame update
     void Start()
     {
-        SetFalse();
+        PageSetFalse();
+
         nextButton.SetActive(false);
         prevButton.SetActive(false);
         inputFinal.SetActive(false);
@@ -99,29 +107,63 @@ public class Scene3Manager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
+        // check if the player has the diary, and if they open it or close it
+        for (int i = 0; i < Inventory.MAX_INVENTORY; i++)
         {
-            player.GetComponent<FirstPersonController>().enabled = false;
-            nextButton.SetActive(true);
-            prevButton.SetActive(true);
-            Cursor.visible = true;
-            Cursor.lockState = CursorLockMode.None;
-            page = 1;
-        }
+            if (Input.GetKeyDown(KeyCode.O) && inven.inventory[i] == diary)
+            {
+                if (diaryOpen)
+                {
+                    player.GetComponent<FirstPersonController>().enabled = true;
+                    PageSetFalse();
+                    Input1False();
+                    Input2False();
+                    Input3False();
+                    ButtonPMFalse();
+                    nextButton.SetActive(false);
+                    prevButton.SetActive(false);
+                    inputFinal.SetActive(false);
+                    // make it so the cursor is invisble
+                    Cursor.visible = false;
+                    diaryOpen = false;
+                    page = 0;
+                }
+                else
+                {
+                    player.GetComponent<FirstPersonController>().enabled = false;
+                    nextButton.SetActive(true);
+                    prevButton.SetActive(true);
+                    // make it so the cursor is visable and can be moved
+                    Cursor.visible = true;
+                    Cursor.lockState = CursorLockMode.None;
+                    diaryOpen = true;
+                    page = 1;
+                }
 
-        if (Input.GetKeyDown(KeyCode.O) || page == 0)
-        {
-            player.GetComponent<FirstPersonController>().enabled = true;
-            SetFalse();
-            Input1False();
-            Input2False();
-            Input3False();
-            ButtonPMFalse();
-            nextButton.SetActive(false);
-            prevButton.SetActive(false);
-            inputFinal.SetActive(false);
-            Cursor.visible = false;
-            page = 0;
+            }
+
+            if (page == 0)
+            {
+                player.GetComponent<FirstPersonController>().enabled = true;
+                PageSetFalse();
+                Input1False();
+                Input2False();
+                Input3False();
+                ButtonPMFalse();
+                nextButton.SetActive(false);
+                prevButton.SetActive(false);
+                inputFinal.SetActive(false);
+                // make it so the cursor is invisble
+                Cursor.visible = false;
+                diaryOpen = false;
+                page = 0;
+            }
+
+
+            if (diary != null && inven.GetCurrentItem() == diary)
+            {
+                DisplayManager.Instance.SetHelpText("Press 'O' to open and close Diary");
+            }
         }
     }
 
@@ -132,22 +174,15 @@ public class Scene3Manager : MonoBehaviour
         {
             float xMin1 = (Screen.width / 2) - pageWidth / 2;
             float yMin1 = (Screen.height / 2) - pageHeight / 2;
-            //GUI.DrawTexture(new Rect(xMin1, yMin1, pageWidth, pageHeight), page1);
-            SetFalse();
-            page1.SetActive(true);
-            inputP1_1.SetActive(true);
-            inputP1_2.SetActive(true);
-            inputP1_3.SetActive(true);
-            inputP1_4.SetActive(true);
-            minusB0.SetActive(true);
-            minusB1.SetActive(true);
-            minusB2.SetActive(true);
-            inputP1_Answer.SetActive(true);
-
+            // make sure what isn't meant to be there isn't
+            PageSetFalse();
             Input2False();
             Input3False();
             inputFinal.SetActive(false);
+            // make sure what is meant to be there is
+            Puzzle1Set();
 
+            // draw the standard texture2ds
             GUI.DrawTexture(new Rect(xMin1 + 30, yMin1 + 700, puzzle1Width, puzzle1Height), puzzle1);
             GUI.DrawTexture(new Rect(xMin1 + 30, yMin1 + 620, mpWidth, mpHeight), minusCode);
             GUI.DrawTexture(new Rect(xMin1 + 300, yMin1 + 620, mpWidth, mpHeight), plusCode);
@@ -156,16 +191,17 @@ public class Scene3Manager : MonoBehaviour
         {
             float xMin2 = (Screen.width / 2) - pageWidth / 2;
             float yMin2 = (Screen.height / 2) - pageHeight / 2;
-            //GUI.DrawTexture(new Rect(xMin2, yMin2, pageWidth, pageHeight), page2);
-            SetFalse();
-            page2.SetActive(true);
-            inputP2_Answer.SetActive(true);
-
+            // make sure what isn't meant to be there isn't
+            PageSetFalse();
             Input1False();
             Input3False();
             inputFinal.SetActive(false);
             ButtonPMFalse();
+            // make sure what is meant to be there is
+            page2.SetActive(true);
+            inputP2_Answer.SetActive(true);
 
+            // draw the standard texture2ds
             GUI.DrawTexture(new Rect(xMin2, yMin2 + 780, puzzle2Width, puzzle2Height), puzzle2);
             GUI.DrawTexture(new Rect(xMin2 + 60, yMin2 + 450, phoneWidth, phoneHeight), phone);
             GUI.DrawTexture(new Rect(xMin2 + 400, yMin2 + 650, signCodeWidth, signCodeHeight), signCode);
@@ -175,19 +211,15 @@ public class Scene3Manager : MonoBehaviour
         {
             float xMin3 = (Screen.width / 2) - pageWidth / 2;
             float yMin3 = (Screen.height / 2) - pageHeight / 2;
-            //GUI.DrawTexture(new Rect(xMin3, yMin3, pageWidth, pageHeight), page3);
-            SetFalse();
-            inputP3_1.SetActive(true);
-            inputP3_2.SetActive(true);
-            inputP3_3.SetActive(true);
-            inputP3_4.SetActive(true);
-            inputP3_Answer.SetActive(true);
-
+            // make sure what isn't meant to be there isn't
+            PageSetFalse();
             Input1False();
             Input2False();
             inputFinal.SetActive(false);
+            // make sure what is meant to be there is
+            Puzzle3Set();
 
-            page3.SetActive(true);
+            // draw the standard texture2ds
             GUI.DrawTexture(new Rect(yMin3 + 680, yMin3 + 700, puzzle3Width, puzzle3Height), puzzle3);
             GUI.DrawTexture(new Rect(xMin3 + 150, yMin3 + 20, p2_1Width, p2_1Height), p2_1);
             GUI.DrawTexture(new Rect(xMin3 + 460, yMin3 + 180, p2_2Width, p2_2Height), p2_2);
@@ -199,15 +231,37 @@ public class Scene3Manager : MonoBehaviour
             Input1False();
             Input2False();
             Input3False();
-            SetFalse();
+            PageSetFalse();
             ButtonPMFalse();
             lastPage.SetActive(true);
             inputFinal.SetActive(true);
         }
     }
+    // set all the properties for puzzle 1 and 2 to true respectivly
+    private void Puzzle1Set()
+    {
+        page1.SetActive(true);
+        inputP1_1.SetActive(true);
+        inputP1_2.SetActive(true);
+        inputP1_3.SetActive(true);
+        inputP1_4.SetActive(true);
+        minusB0.SetActive(true);
+        minusB1.SetActive(true);
+        minusB2.SetActive(true);
+        inputP1_Answer.SetActive(true);
+    }
 
-
-    private void SetFalse()
+    private void Puzzle3Set()
+    {
+        inputP3_1.SetActive(true);
+        inputP3_2.SetActive(true);
+        inputP3_3.SetActive(true);
+        inputP3_4.SetActive(true);
+        inputP3_Answer.SetActive(true);
+        page3.SetActive(true);
+    }
+    // set all the pages to false
+    private void PageSetFalse()
     {
         page1.SetActive(false);
         page2.SetActive(false);
@@ -215,6 +269,7 @@ public class Scene3Manager : MonoBehaviour
         lastPage.SetActive(false);
     }
 
+    // set all the inputs of puzzle 1 to false
     private void Input1False()
     {
         inputP1_1.SetActive(false);
@@ -223,12 +278,12 @@ public class Scene3Manager : MonoBehaviour
         inputP1_4.SetActive(false);
         inputP1_Answer.SetActive(false);
     }
-
+    // set all the inputs of puzzle 2 to false
     private void Input2False()
     {
         inputP2_Answer.SetActive(false);
     }
-
+    // set all the inputs of puzzle 3 to false
     private void Input3False()
     {
         inputP3_1.SetActive(false);
@@ -238,7 +293,18 @@ public class Scene3Manager : MonoBehaviour
         inputP3_Answer.SetActive(false);
     }
 
+    // set all the plus and minus buttons to false
+    private void ButtonPMFalse()
+    {
+        plusB0.SetActive(false);
+        plusB1.SetActive(false);
+        plusB2.SetActive(false);
+        minusB0.SetActive(false);
+        minusB1.SetActive(false);
+        minusB2.SetActive(false);
+    }
 
+    // when the user presses a "-" or "+" switch it to the other
     public void SwitchSign0()
     {
         if (minusB0.activeSelf)
@@ -280,19 +346,11 @@ public class Scene3Manager : MonoBehaviour
         }
     }
 
-    private void ButtonPMFalse()
-    {
-        plusB0.SetActive(false);
-        plusB1.SetActive(false);
-        plusB2.SetActive(false);
-        minusB0.SetActive(false);
-        minusB1.SetActive(false);
-        minusB2.SetActive(false);
-    }
 
+    // code for handeling all of the puzzle answers in input fields
     public void Puzzl1Answer(string answer)
     {
-        if (answer == "8")
+        if (answer == "08")
         {
             nextPage();
         }
@@ -318,9 +376,12 @@ public class Scene3Manager : MonoBehaviour
     {
         if (answer == "08/17/97")
         {
+            // close the diary
             page = 0;
+            inven.RemoveCurrentItem();
         }
     }
+    // go to the next or prev page when the buttons are clicked
     public void nextPage()
     {
         if (page == 4)
@@ -344,4 +405,6 @@ public class Scene3Manager : MonoBehaviour
             page--;
         }
     }
+
+    
 }
