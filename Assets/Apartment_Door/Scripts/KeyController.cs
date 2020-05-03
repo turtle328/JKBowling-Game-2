cac308d05a2e10fb71ff4640f9bda927cdd8d61e
ajr6974 <ajr6974@g.rit.edu>
2020-05-02 22:39:40 -0400

    added first puzzle

---
+++ Assets/Apartment_Door/Scripts/KeyController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyController : MonoBehaviour
+{
+    Rigidbody keyRB;
+    public Text txtToDisplay;
+    public DoorController DC;
+
+    public void GiveKey()
+    {
+        DC.gotKey = true;
+        txtToDisplay.gameObject.SetActive(true);
+        txtToDisplay.text = "Kitchen Key Acquired";
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/TreeRockCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeRockCollision : MonoBehaviour
+{
+    // rock reference
+    public GameObject rock;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == rock)
+        {
+            PuzzleManagerWorld1.Instance.DropKey();
+        }
+    }
+}
