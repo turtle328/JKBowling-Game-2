9e44cc80a03a0b8b542350e5cdfc864537185f47
ajr6974 <ajr6974@g.rit.edu>
2020-05-03 18:24:55 -0400

    Fixed crosshair and text overlap. Crosshair is disabled when text alpha > 0

--- Assets/Scripts/CrossHair.cs
+++ Assets/Scripts/CrossHair.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CrossHair : MonoBehaviour
 {
     public Texture2D crosshairImage;
+    public TextMeshProUGUI text;
     private int crosshairSize = 25;
 
     void OnGUI()
     {
-        float xMin = (Screen.width / 2) - (crosshairSize / 2);
-        float yMin = (Screen.height / 2) - (crosshairSize / 2);
-        GUI.DrawTexture(new Rect(xMin, yMin, crosshairSize, crosshairSize), crosshairImage);
+        if (text.alpha <= 0)
+        {
+            float xMin = (Screen.width / 2) - (crosshairSize / 2);
+            float yMin = (Screen.height / 2) - (crosshairSize / 2);
+            GUI.DrawTexture(new Rect(xMin, yMin, crosshairSize, crosshairSize), crosshairImage);
+        }
     }
 }
